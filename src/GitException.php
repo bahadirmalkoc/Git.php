c85d87eb9300cd5bdde6fa205e7c476f37fbc50d
bahadirmalkoc <baham1986@yahoo.com>
2016-10-27 07:06:20 +0300

    Added .gitignore Namespaced PHPUnit to composer

---
+++ src/GitException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pub\Git;
+
+use Exception;
+
+/**
+ * Class GitException is used for exceptions thrown from this library.
+ *
+ * @package Pub\Git
+ */
+class GitException extends Exception {
+    
+}
